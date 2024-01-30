9a3d7d0a30c5bb0f12a33eb611a0eeb372b8ef46
Maria Villalobos <villapadita1@gmail.com>
2024-01-29 22:25:31 -0400

    async await

--- pokemon.js
+++ pokemon.js
@@ -4,3 +4,11 @@ const url = 'https://pokeapi.co/api/v2/pokemon/pikachu'
     .then((response) => response.json())
     .then( (data) => console.log(data.name))
     .catch(error => console.log(error))
+
+async function asyncPoke() {
+    let response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+    return response.json()
+}
+
+asyncPoke()
+.then(data => console.log(data.name))
